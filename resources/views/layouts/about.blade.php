2e0b23ca76d4cf001e701759b8480e2d7c62b6ae
momtajhusen <mumtaz666raza@gmail.com>
2024-02-29 20:45:52 +0545

    update about page

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -1,6 +1,12 @@
 @extends('teplate')
 
 @section('style')
+  <style>
+     body{
+          background: #f2f2f2;
+     }
+
+  </style>
     
 @endsection
 
@@ -9,5 +15,16 @@
 @endsection
 
 @section('contents')
-     <h1>About</h1>
+       <div class="border p-5 d-flex align-items-center flex-column">
+             <h1>ABOUT US</h1>
+             <p class="text-center" style="font-size:20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque <br> ac  lacus eget nunc imperdiet</p>
+             <img style="width:1000px;" src="https://demo.htmlcodex.com/494/it-company-website-template/img/about.jpg" alt="Image 3">
+       </div>
+        <div class="position-relative d-flex justify-content-center border w-100">
+            <div class="border w-50 p-3" style="position: absolute; top:-150px; background:#ffffff;">
+                <h3>Welcome to Our Site</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum rutrum ligula. Integer ac porttitor mi. In finibus vehicula aliquet. Vestibulum et velit placerat pretium lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum rutrum ligula. Integer ac porttitor mi. In finibus vehicula aliquet. Vestibulum et velit placerat pretium lorem</p>
+                <button type="button" class="btn btn-outline-secondary">Read More</button>
+            </div>
+        </div>
 @endsection
