e2fab3e14e580e6702f6e294fec758d6d208fd39
Er.Bijay sharma <sharmabijay603@gmail.com>
2024-02-29 20:30:21 +0545

    ds

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -16,9 +16,27 @@
 
   <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="2000">
-      <img src="{{ asset('assest/image/slider-1.png')}}" class="d-block w-25" alt="...">
-    </div>
+    <div class="carousel-item active" data-bs-interval="3000">  
+    <div class="container">
+    <div class="row">
+          <div class="col-6 text-center">
+          <p class="h1">Welcome to Script Qube</p>     
+          </div>
+     </div>
+     <div class="row">
+          <div class="col-7 text text-script">
+          At Script Qube, we're more than just a software company
+          </div>
+     </div>
+     <div class="row">
+          <div class="col-6">
+          <button type="button" class="btn-info read_more_btn" >Read More</button>
+          </div>
+     </div>
+     <img src="{{ asset('assest/image/slider-1.png')}}" class="d-block w-25" alt="...">
+       
+</div>
+  </div>
     <div class="carousel-item" data-bs-interval="2000">
       <img src="{{ asset('assest/image/slider-2.png')}}" class="d-block w-25" alt="...">
     </div>
@@ -34,9 +52,6 @@
     <span class="carousel-control-next-icon" aria-hidden="false"></span>
     <span class="visually-show">Next</span>
   </button>
-</div>
-<div class="container">
-hello
 </div>
 
 <!-- 
