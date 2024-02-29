c0b22c0379c2ed83b97063fe292c0cdd5954da3d
momtajhusen <mumtaz666raza@gmail.com>
2024-02-29 22:18:20 +0545

    update

--- resources/views/teplate.blade.php
+++ resources/views/teplate.blade.php
@@ -10,7 +10,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
-    <title>Document</title>
+    <title>Master ScriptQube</title>
     @yield('style')
 
     <style>
