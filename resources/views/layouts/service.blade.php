1a3432cdd4e85fc7d807415e6c84c766d4c8b896
Er.Bijay sharma <sharmabijay603@gmail.com>
2024-04-01 18:09:39 +0545

    update

--- resources/views/layouts/service.blade.php
+++ resources/views/layouts/service.blade.php
@@ -9,5 +9,32 @@
 @endsection
 
 @section('contents')
-     <h1>Service</h1>
+  <div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+     <div clase="row">
+     <div clase="col-12">
+      <h1 class="display-6 display-md-4 mt-2">Welcome to ScriptQube IT Solutions</h1>
+      <p class="lead display-0 display-md-5">We provide innovative IT solutions to help your business succeed.</p>
+     </div>
+     </div>
+     </div>
+  </div>
+
+  <div id="services" class="container">
+    <h2 class="text-center " style="color:#042954;">Our Services</h2>
+    <div class="row"> 
+      <div class="col-lg-4 col-12">
+        <h3 clase="text-center borders">Web Development</h3>
+        <p clase="text-center">Custom, responsive websites tailored to meet your business needs.</p>
+      </div>
+      <div class="col-lg-4">
+        <h3>Mobile App Development</h3>
+        <p>Native and cross-platform mobile applications for iOS and Android.</p>
+      </div>
+      <div class="col-lg-4">
+        <h3>Software Development</h3>
+        <p>Full lifecycle application development with cutting-edge technologies.</p>
+      </div>
+    </div>
+  </div>
 @endsection
