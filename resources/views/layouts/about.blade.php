42b2e8bed80201cc948385dae5ea6ea95fe906c2
Er.Bijay sharma <sharmabijay603@gmail.com>
2024-04-01 09:07:59 +0545

    update about text

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -11,14 +11,18 @@
 
 @section('contents')
        <div class="border p-5 d-flex align-items-center flex-column">
-             <h1>ABOUT US</h1>
-             <p class="text-center" style="font-size:20px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque <br> ac  lacus eget nunc imperdiet</p>
+             <h1>Welcome to scriptQube</h1>
+             <p class="text-center" style="font-size:20px;">
+             At Script Qube, we're more than just a software company
+            </p>
              <img style="width:1000px;" src="https://demo.htmlcodex.com/494/it-company-website-template/img/about.jpg" alt="Image 3">
        </div>
-        <div class="position-relative d-flex justify-content-center border w-100">
+        <div class="position-relative d-flex justify-content-center border w-100 bg-blue">
             <div class="border w-50 p-3" style="position: absolute; top:-150px; background:#ffffff;">
                 <h3>Welcome to Our Site</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum rutrum ligula. Integer ac porttitor mi. In finibus vehicula aliquet. Vestibulum et velit placerat pretium lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras condimentum rutrum ligula. Integer ac porttitor mi. In finibus vehicula aliquet. Vestibulum et velit placerat pretium lorem</p>
+                <p>
+                At Script Qube, we're more than just a software company; we're your trusted partner in navigating the ever-evolving landscape of technology. Since our establishment in 2022, we've been committed to delivering innovative IT solutions and cutting-edge AI technologies to clients both in Nepal and across the globe
+                </p>
                 <button type="button" class="btn btn-outline-secondary">Read More</button>
             </div>
         </div>
