266a615990bfb5fafa6066cb865a08befbaa9240
Er.Bijay sharma <sharmabijay603@gmail.com>
2024-04-01 09:26:20 +0545

    new rout

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,5 @@ Route::view('/home', 'layouts/home')->name('home');
 Route::view('/about', 'layouts/about')->name('about');
 Route::view('/portfolio', 'layouts/portfolio')->name('portfolio');
 Route::view('/service', 'layouts/service')->name('service');
-Route::view('/portfolio', 'layouts/portfolio')->name('portfolio');
 Route::view('/pricing', 'layouts/pricing')->name('pricing');
 Route::view('/contact', 'layouts/contact')->name('contact');
-
-
-
-
-
-
- 
