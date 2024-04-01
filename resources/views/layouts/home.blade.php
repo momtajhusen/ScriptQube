da4e770d982a598ba68e9d3b5ed77a33f7570731
Er.Bijay sharma <sharmabijay603@gmail.com>
2024-04-01 08:52:15 +0545

    i have update new port

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,96 +1,95 @@
 @extends('teplate')
 
 @section('style')
-<link rel="stylesheet" href="{{asset('assest/style/home.css')}}"> 
 @endsection
 
-@section('script')
-     <script src="{{asset('assest/script/home.js')}}"></script>
-     
+@section('script')     
 @endsection
 
 @section('contents')
-<div class="slider-container">
-  
+ <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="{{ asset('assest/image/school baner.jpg')}}" class="d-block w-100 col-12" alt="school baner">
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('assest/image/school baner.jpg')}}" class="d-block w-100" alt="school baner">
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('assest/image/school baner.jpg')}}" class="d-block w-100" alt="school baner">
+          </div>
+        </div>
+</div>
+<!-- here we start company mission  -->
+<div class="container bg-blue">
+<div class="row">
+            <div class="col-12">
+                <button class="btn text-white script-font-family mt-1 text-capitalize" style="font-size: 35px;">
+                    Our Mission
+                </button>
+                <p class="text-white ms-3 text-justify text-capitalize script-font-family" style="font-size: 18px;">
+                    Our mission at Script Qube is simple: to empower businesses and institutions with transformative technology solutions that streamline operations, enhance productivity, and drive growth. We strive to be at the forefront of technological advancements, continuously pushing boundaries to provide our clients with the most effective tools to succeed in today's digital age.
+                </p>
+            </div>
 
-  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="3000">  
-    <div class="container">
-    <div class="row">          <div class="col-6 text-center">
-          <p class="h1">Welcome to Script Qube</p>     
-          </div>
-     </div>
-     <div class="row">
-          <div class="col-7 text text-script">
-          At Script Qube, we're more than just a software company
-          </div>
-     </div>
-     <div class="row">
-          <div class="col-6">
-          <button type="button" class="btn-info read_more_btn" >Read More</button>
-          </div>
-     </div>
-     <img src="{{ asset('assest/image/slider-1.png')}}" class="d-block w-25" alt="...">
-       
+        </div>
 </div>
-  </div>
-    <div class="carousel-item" data-bs-interval="2000">
-    <div class="container">
+<div class="container">
     <div class="row">
-          <div class="col-6 text-center">
-          <p class="h1">Welcome to Script Qube</p>     
-          </div>
-     </div>
-     <div class="row">
-          <div class="col-7 text text-script">
-          At Script Qube, we're more than just a software company
-          </div>
-     </div>
-     <div class="row">
-          <div class="col-6">
-          <button type="button" class="btn-info read_more_btn" >Read More</button>
-          </div>
-     </div>     
-     <img src="{{ asset('assest/image/slider-2.png')}}" class="d-block w-25" alt="...">
-</div>
-   
+        <div class="col-12 col-md-12 d-flex justify-content-center" style="height: 300px;">
+            <div class="rounded-circle mt-2 bg-blue" style="width: 250px; height: 250px;">
+            </div>
+        </div>
     </div>
-    <div class="carousel-item">
+</div>
+<div class="container w-100 h-100">
+    <div class="row">
+        <div class="col-12">
+            <p class="text-center display-3" style="font-weight:bold; color:#042954;">Ours Clients</p>
+        </div>
+     </div>  
+        <div class="row">
+             <div class="col-md-6 col-12 d-flex justify-content-center">
+                     <div class="" style="border:10px solid #042954; width:400px; height:300px;">
 
-    <div class="container">
-    <div class="row">
-          <div class="col-6 text-center">
-          <p class="h1">Welcome to Script Qube</p>     
-          </div>
-     </div>
-     <div class="row">
-          <div class="col-7 text text-script">
-          At Script Qube, we're more than just a software company
-          </div>
-     </div>
-     <div class="row">
-          <div class="col-6">
-          <button type="button" class="btn-info read_more_btn" >Read More</button>
-          </div>
-     </div>    
-     <img src="{{ asset('assest/image/slider-3.png')}}" class="d-block w-25" alt="..."> 
-</div>
-    
+                         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                              <div class="carousel-inner">
+                                   <div class="carousel-item active" style="height:250px;">
+                                        <img src="{{ asset('assest/image/team-1.jpg')}}" class="d-block w-100 " alt="school-1">
+                                   </div>
+                                   <div class="carousel-item" style="height:250px;">
+                                        <img src="{{ asset('assest/image/team-1.jpg')}}" class="d-block w-100"  alt="schppl-2">
+                                   </div>
+                                   <div class="carousel-item" style="height:250px;">
+                                        <img src="{{ asset('assest/image/team-1.jpg')}}" class="d-block w-100 "alt="school-2">
+                                   </div>
+                              </div>
+                         </div>
+
+                    </div>
+        
+             </div>
+             <div class="col-md-6 col-12 d-flex justify-content-center">
+                    <div class="" style="border:10px solid #042954; width:400px; height:300px;">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                              <div class="carousel-inner">
+                                   <div class="carousel-item active" style="height:250px;">
+                                        <img src="{{ asset('assest/image/team-1.jpg')}}" class="d-block w-100 " alt="school-1">
+                                   </div>
+                                   <div class="carousel-item" style="height:250px;">
+                                        <img src="{{ asset('assest/image/team-1.jpg')}}" class="d-block w-100"  alt="schppl-2">
+                                   </div>
+                                   <div class="carousel-item" style="height:250px;">
+                                        <img src="{{ asset('assest/image/team-1.jpg')}}" class="d-block w-100 "alt="school-2">
+                                   </div>
+                              </div>
+                         </div>
+
+                    </div>
+             </div>
+          
     </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="false"></span>
-    <span class="visually-show">Next</span>
-  </button>
 </div>
-<!-- 
-<button class="prev">&#10094;</button>
-  <button class="next">&#10095;</button>
-</div>-->
+<!-- here we end company mission  -->
 @endsection
 
--- resources/views/teplate.blade.php
+++ resources/views/teplate.blade.php
@@ -1,59 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="style.css" rel="stylesheet">
+    <link href="responsive.css" rel="stylesheet">
+    <script src="javascript/responsive_javascript.js"></script>
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- this cdn is google fafa icon  -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oleo+Script:wght@400;700&display=swap" rel="stylesheet">
 
-        <!-- Bootstrap core CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-
-    <title>Document</title>
+    <title>ScriptQube</title>
     @yield('style')
 
     <style>
-      body{
+       body{
            background: #f2f2f2;
       }
-   </style>
-
+        .borders {
+            border: 1px solid red;
+        }
+        .bg-blue{
+            background-color: #042954;
+        }
+        .script-font-family{
+            /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
+            /* font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; */
+            font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            /* font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; */
+            /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
+            /* font-family: Arial, Helvetica, sans-serif; */
+            /* font-family: Arial, Helvetica, sans-serif; */
+            /* font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif */
+        }
+    </style>
+    
 </head>
 <body>
-
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-4">
+<div class="jumbotron bg-blue">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">SCRIPTQUBE Momtaj</a>
-          <button class="navbar-toggler ml-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-            <!-- Align menu items to the right -->
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{route('home')}}">HOME</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('about')}}">ABOUT</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('service')}}">SERVICE</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('portfolio')}}">PORTFOLIO</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('pricing')}}">PRICING</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{route('contact')}}">CONTACT</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand text-white ms-2 " style="font-size: 40px; font-weight: 500;" href="{{route('home')}}">ScriptQube</a>
+            <div class="me-1 pe-3">
+                <button class="navbar-toggler bg-white" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav gap-1 script-font-family ms-auto" style="font-size:25px;">
+                    <li class="nav-item ">
+                      <a class="nav-link text-white h-100" aria-current="page" href="{{route('home')}}">Home</a>
+                    </li>
+                    <li class="nav-item "><a href="{{route('about')}}" class="nav-link text-white h-100" >About</a></li>
+                    <li class="nav-item "><a href="{{route('service')}}" class="nav-link text-white h-100" >Service</a></li>
+                    <li class="nav-item "><a href="{{route('portfolio')}}" class="nav-link text-white h-100" >Portfolio</a></li>
+                    <li class="nav-item "><a href="{{route('pricing')}}" class="nav-link text-white h-100" >Pricing</a></li>
+                    <li class="nav-item "><a href="{{route('contact')}}"class="nav-link text-white h-100" >Contact</a></li>
+                </ul>
+            </div>
+            
+        </nav>
+      </div>
+      </div>
       @yield('contents')
 
     @yield('script')
